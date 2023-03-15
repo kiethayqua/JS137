526530d3a3a6877304d828d6fc51b63304087570
kiet.nguyen2@mservice.com.vn <kiet.nguyen2@mservice.com.vn>
2023-03-15 19:47:58 +0700

    init day 6

---
+++ day6/index.js
@@ -0,0 +1,57 @@
+/* Phần 1: biến, kiểu dữ liệu, toán tử cơ bản */
+
+// Khai báo biến thể hiện họ của anh/chị
+
+// khai báo biến thể hiện tên của anh/chị 
+
+// Khai báo biến thể hiện tuổi của anh/chị
+
+// Tăng tuổi của anh chị lên 1 đơn vị và in ra màn hình
+
+// Dùng cú pháp template string để in ra nội dung "<họ> <tên> - <tuổi>"
+
+// Khai báo biến kiểu boolean thể hiện anh/chị có phải là Developer hay không
+
+
+/* Phần 2: Phép toán */
+
+// Tạo 2 biến a và b là 2 số
+
+// Cộng 2 số a và b và in ra kết quả
+
+// Lấy số a trừ số b và in ra kết quả
+
+// Bình phương số a và in ra kết quả
+
+// Khai báo biến c = 5 và chia lấy dư c với 2 sau đó in ra kết quả
+
+// Khai báo biến d bằng một số bất kì, dùng toán tử ++ để tăng d lên 1 đơn vị, sau đó in ra kết quả
+
+// Khai báo biến e bằng một số bất kì, dùng phép gán -= để giảm biến e đi 2 đơn vị, sau đó in ra kết quả
+
+
+/* Phần 3: Toán tử so sánh */
+
+// Khai báo biến number1 = 5 và number2 = "5"
+
+// In ra màn hình kết quả so sánh number1 == number2
+
+// In ra màn hình kết quả so sánh number1 === number2
+
+// Giải thích sự khác nhau giữa so sánh == và ===
+
+// Khai báo 2 biến x = true, y = false
+
+// In ra màn hình kết quả của logic x && y
+
+// In ra màn hình kết qủa của logic x || y
+
+// Giải thích sự khác nhau giữa && và ||
+
+// In ra màn hình kết quả của logic x && !y
+
+// Giải thích tại sao x && !y lại cho ra kết qủa true
+
+
+
+
