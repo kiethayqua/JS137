0a879dfab53080f598584cdfb9877fd10187d790
toolong <thienlongphambao@gmail.com>
2023-03-22 16:07:30 +0700

    done but 10

--- day7/practice1.js
+++ day7/practice1.js
@@ -1,27 +1,55 @@
-// tạo biến age với giá trị tuổi của bạn
+// // // tạo biến age với giá trị tuổi của bạn
+// // const myAge = 26;
+// // console.log(myAge);
+// // // tạo biến name với giá trị là tên của bạn
+// // const myName = "Long";
+// // // tạo biến greeting với giá trị là "Hello, My name is <name>, <age> years old!", in ra kết quả
+// // const greeting = `Hello, my name is ${myName}, ${myAge} years old`;
+// // console.log(greeting);
+// // // Kiểm tra trong biến greeting có tồn tại từ "Hello" hay không, không phân biệt chữ hoa chữ thường, in ra kết quả
+// // console.log(
+// //   "check Hello =",
+// //   greeting.toLowerCase().includes("Hello".toLowerCase())
+// // );
+// // // Tạo biến sayHi thay thể "Hello" trong greeting thành "Hi", in ra màn hình
+// // const sayHi = greeting.replace("Hello", "Hi");
+// // console.log(sayHi);
+// // /*
+// // Tạo biến mark = 9.7 và thực hiện:
+// // - Làm tròn thành 10 và in ra
+// // - Làm tròn về 9 và in ra
+// // */
+// // const mark = 9.7;
+// // console.log(Math.ceil(mark));
+// // console.log(Math.floor(mark));
 
-// tạo biến name với giá trị là tên của bạn
+// // // Tạo 1 biến lưu độ dài cạnh của hình vuông và tính diện tích hình vuông đó sau đó in ra màn hình
+// // const edge = 5;
+// // console.log(edge ** 2);
 
-// tạo biến greeting với giá trị là "Hello, My name is <name>, <age> years old!", in ra kết quả
-
-// Kiểm tra trong biến greeting có tồn tại từ "Hello" hay không, không phân biệt chữ hoa chữ thường, in ra kết quả
-
-// Tạo biến sayHi thay thể "Hello" trong greeting thành "Hi", in ra màn hình
-
-/*
-Tạo biến mark = 9.7 và thực hiện:
-- Làm tròn thành 10 và in ra
-- Làm tròn về 9 và in ra
-*/
-
-// Tạo 1 biến lưu độ dài cạnh của hình vuông và tính diện tích hình vuông đó sau đó in ra màn hình
-
-/*
-Viết cấu trúc điều kiện kiểm tra độ tuổi lái xe với điều kiện sau: 
-- Nếu lớn hơn hoặc bằng 18 tuổi in ra màn hình: "Bạn đã đủ tuổi lái xe"
-- Nếu nhỏ hơn 18 tuổi in ra màn hình: "Bạn chưa đủ tuổi lái xe"
-*/
-
-/*
-Viết cấu trúc switch-case kiểm tra số chẳn và lẻ
-*/
+// /*
+// Viết cấu trúc điều kiện kiểm tra độ tuổi lái xe với điều kiện sau:
+// - Nếu lớn hơn hoặc bằng 18 tuổi in ra màn hình: "Bạn đã đủ tuổi lái xe"
+// - Nếu nhỏ hơn 18 tuổi in ra màn hình: "Bạn chưa đủ tuổi lái xe"
+// */
+// const age = prompt("nhap do tuoi cua ban");
+// if (age >= 18) {
+//   console.log("Ban da du tuoi lai xe");
+// } else {
+//   console.log("ban chua du tuoi lai xe");
+// }
+// /*
+// Viết cấu trúc switch-case kiểm tra số chẳn và lẻ
+// */
+// const numberInput = Number(prompt("Nhap so:"));
+// switch (numberInput % 2) {
+//   case 0:
+//     alert(`${numberInput} la so chan`);
+//     break;
+//   case 1:
+//     alert(`${numberInput} la so le`);
+//     break;
+//   default:
+//     alert("Khong phai so");
+//     break;
+// }
--- day7/practice2.js
+++ day7/practice2.js
@@ -1,18 +1,53 @@
-// Bài 1: In ra màn hình các số từ 1..100 theo thứ tự tăng dần
+// // Bài 1: In ra màn hình các số từ 1..100 theo thứ tự tăng dần
+// for (let i = 1; i <= 100; i++) {
+//   console.log(i);
+// }
 
 // Bài 2: In ra màn hình các số từ 1..100 theo thứ tự giảm dần
+// for (let i = 100; i >= 1; i--) {
+//   console.log(i);
+// }
 
 // Bài 3: Cho số nguyên n bất kì, in các số chẵn từ 1..n
+// const n = prompt("nhap so n:");
+// for (let i = 1; i <= n; i++) {
+//   if (i % 2 === 0) console.log(i);
+// }
 
 // Bài 4: Cho số nguyên n bất kì, in các số lẻ từ 1..n
+// const n = prompt("nhap so n:");
+// for (let i = 1; i <= n; i++) {
+//   if (i % 2 !== 0) console.log(i);
+// }
 
 // Bài 5: Cho số nguyên n bất kì, tính tổng các số từ 1..n
+// const n = prompt("Nhap so n:");
+// let sum = 0;
+// for (let i = 0; i <= n; i++) {
+//   sum += i;
+// }
+// console.log(sum);
 
 // Bài 6: Cho số nguyên n. Tính giá trị biểu thức S= 1.2 + 2.3 + 3.4 + ... + n(n+1).
+// const n = prompt("Nhap so n:");
+// let s = 0;
+// for (let i = 0; i <= n; i++) {
+//   s += i * (i + 1);
+// }
+// console.log(s);
 
 // Bài 7: Cho các số nguyên n và m bất kì, tính n^m VD: 2^3 = 8
+// const n = prompt("Nhap so n:");
+// const m = prompt("Nhap so m:");
+// console.log(Math.pow(n, m));
 
 // Bài 8: Cho số nguyên n bất kì, tính n! VD: 3! = 1*2*3 = 6
+// const n = prompt("Nhap so n:");
+// let f = 1;
+// for (let i = 1; i <= n; i++) {
+//   f *= i;
+// }
+// console.log(f);
 
 // Bài 9: Vẽ tam giác có dạng sau:
 // *****
@@ -20,7 +55,13 @@
 // ***
 // **
 // *
-
+// for (let i = 5; i > 0; i--) {
+//   let row = "";
+//   for (let j = 0; j < i; j++) {
+//     row += "*";
+//   }
+//   console.log(row);
+// }
 
 // Bài 10: Xây dựng chương trình game đoán số từ 0..9(số được random)
 // - Cho phép nhập từ trình duyệt với nội dung - 'Nhập số: '
