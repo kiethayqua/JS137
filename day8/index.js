8c2718d43090897fb6839e84aca302d0c8adf740
HaoDuong1994 <hao.dv5370@gmail.com>
2023-03-25 16:17:35 +0700

    not done yet

--- day8/index.js
+++ day8/index.js
@@ -1,5 +1,10 @@
 // Find the sum of all elements.
 const arr1 = [1, 2, 3, 4, 5];
+// let result=0;
+// for( let i =0; i<arr1.length;i++){
+//     result= result+i
+// }
+// console.log(result);
 
 // Remove duplicates from an array.
 let arr2 = [1, 2, 2, 4, 5, 5, 6];
@@ -10,15 +15,40 @@ const arr3 = ['iOS', 'Android', 'WindowPhone'];
 // Reverse an array.
 const arr4 = [1, 2, 3, 4, 5, 6];
 
+
 // Filter out even numbers from an array.
-const arr5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const arr5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for(let i=0;i<arr5.length;i++){
+//     if(arr5[i]%2==0){console.log(i+'.'+arr5[i])
+//     }
+// }
 
 // Find the maximum and minimum values in an array of numbers.
 const arr6 = [1, -3, 99, 120, 50, 15];
+//Tính min
+// let min = arr6[0]
+// for( let i= 0;i<arr6.length;i++){
+//     if(min>arr6[i]){min=arr6[i]}
+// }
+// console.log(min);
+
+//Tính max
+// let max = arr6[0]
+// for(let i=0; i<arr6.length;i++){
+//     if(max<arr6[i]){max=arr6[i]};
+// }
+// console.log(max);
 
 // Find the common elements between two arrays.
 const arr7 = [1, 2, 3, 5, 9];
 const arr8 = [2, 4, 1, 9, 0];
 
 // Map an array of numbers to their squares.
-const arr9 = [1, 2, 3, 4, 5];
+ const arr9 = [1, 2, 3, 4, 5];
+// let maparr9 = arr9.map(function(n){
+//     return n*n
+// })
+// console.log(maparr9);
+// arr9.forEach(function(n){
+//     console.log(n);
+// })
