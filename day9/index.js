9f0b872a70db124bf97162998d32948d3522c99e
Taylortran96 <tranaithy96@gmail.com>
2023-03-26 22:35:36 +0700

    Update index.js

--- day9/index.js
+++ day9/index.js
@@ -22,7 +22,6 @@
     
 */
 
-
 const developers = [
     {
         name: "Nguyễn Văn Tí",
@@ -61,3 +60,43 @@ const developers = [
         skills: ['Assembly', 'C', 'C++']
     }
 ];
+
+// 1/ Viết function tìm ra developer lớn tuổi nhất
+function maxAge ()
+// 2/ Viết function trả về danh sách developer đã được sắp xếp theo tuổi từ thấp đến cao
+Array.sort()
+
+// 3/ Viết function tìm ra những developer nam
+Array.filter (gender = "male")
+// 4/ Viết function tính tổng tuổi của tất cả developer
+Array.reduce
+
+// 5/ Viết function tổng hợp tất cả các skills của tất cả developers trong mảng
+// Array.Set (add vào k bị trùng, bị trùng thì loại)
+
+// 6/ Viết function tìm ra developer có nhiều skills nhất
+//Array.length
+
+// 7/ Viết function tìm ra những developer nào có skill là JS
+//Quét qua 1 lượt và Array.filter + includes
+
+
+// 8/ Viết function tìm ra những developer có gender là male và có skill JS
+//Kết hợp câu 3 và câu 7
+
+// 9/ Viết function sắp xếp thứ tự developer có nhiều skill nhất -> ít nhất
+//array.length
+
+
+// 10/ Viết function trả về 1 object có dạng 
+/*{
+    males: [{...}],
+    females: [{...}]
+}
+Trong đó:
+    + males là mảng tất cả các developers có gender là male
+    + females là mảng tất cả các developers có gender là female
+
+*/
+//Tạo 2 biến male và female 
+// dùng filter và reduce
