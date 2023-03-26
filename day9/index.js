ca5477042efdb2734070987c0b78c1f7d9cd97fb
Taduynguyen <taduynguyen11@gmail.com>
2023-03-26 22:34:57 +0700

    Update index.js

--- day9/index.js
+++ day9/index.js
@@ -4,22 +4,22 @@
     Cho 1 mảng có dạng:
     - 1/ Viết function tìm ra developer lớn tuổi nhất
     - 2/ Viết function trả về danh sách developer đã được sắp xếp theo tuổi từ thấp đến cao
-    - 3/ Viết function tìm ra những developer nam
-    - 4/ Viết function tính tổng tuổi của tất cả developer
-    - 5/ Viết function tổng hợp tất cả các skills của tất cả developers trong mảng
-    - 6/ Viết function tìm ra developer có nhiều skills nhất
-    - 7/ Viết function tìm ra những developer nào có skill là JS
-    - 8/ Viết function tìm ra những developer có gender là male và có skill JS
-    - 9/ Viết function sắp xếp thứ tự developer có nhiều skill nhất -> ít nhất
+    - 3/ Viết function tìm ra những developer nam -filter
+    - 4/ Viết function tính tổng tuổi của tất cả developer - reduce
+    - 5/ Viết function tổng hợp tất cả các skills của tất cả developers trong mảng - 
+    - 6/ Viết function tìm ra developer có nhiều skills nhất -.lengh
+    - 7/ Viết function tìm ra những developer nào có skill là JS - filter -include
+    - 8/ Viết function tìm ra những developer có gender là male và có skill JS - &&
+    - 9/ Viết function sắp xếp thứ tự developer có nhiều skill nhất -> ít nhất - lengh
     - 10/ Viết function trả về 1 object có dạng 
     {
         males: [{...}],
-        females: [{...}]
+        females: [{...}] 
     }
     Trong đó:
         + males là mảng tất cả các developers có gender là male
         + females là mảng tất cả các developers có gender là female
-    
+    - reduce
 */
 
 
