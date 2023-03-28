f742ea31671689e073873cfa5977bc25c1850fd4
NTDiShark <datnguyen2298@gmail.com>
2023-03-28 21:58:31 +0700

    except question no.10

--- day9/index.js
+++ day9/index.js
@@ -61,3 +61,68 @@ const developers = [
         skills: ['Assembly', 'C', 'C++']
     }
 ];
+
+//     - 1/ Viết function tìm ra developer lớn tuổi nhất
+// function oldestDev() {
+//     let _oldestDev = developers[0];
+    // for (let i = 1; i < developers.length; i++)
+    //     if(developers[i]['age'] > _oldestDev['age']) _oldestDev = developers[i];
+    // console.log(_oldestDev);
+// }
+// oldestDev();
+
+//     - 2/ Viết function trả về danh sách developer đã được sắp xếp theo tuổi từ thấp đến cao
+// console.log(developers.sort(function(a,b){return a['age']-b['age'];}));
+
+//     - 3/ Viết function tìm ra những developer nam => dùng filter
+// const checkGender = (value)  => {
+//     return value['gender'] == "male";
+// };
+// console.log(developers.filter(checkGender));
+
+
+//     - 4/ Viết function tính tổng tuổi của tất cả developer
+// function totalAge() {
+//     let sum=0;
+//     for(let i=0; i<developers.length; i++) sum += developers[i]['age'];
+//     alert(sum);
+// }
+// totalAge();
+
+//     - 5/ Viết function tổng hợp tất cả các skills của tất cả developers trong mảng
+// const uniqueSkill = (arr1, arr2) => {
+//     for(let i=0; i<arr2.length; i++)
+//         if(!arr1.includes(arr2[i])) arr1.push(arr2[i]);
+//     return arr1;
+// };
+// const allSkills = () => {
+//     let tempArr = [];
+//     for(let i=0; i<developers.length; i++)
+//         uniqueSkill(tempArr, developers[i]['skills']);
+//     console.log(tempArr);
+// }
+// allSkills();
+
+//     - 6/ Viết function tìm ra developer có nhiều skills nhất
+// function mostSkill() {
+//     let _mostSkill = developers[0]
+//     for (let i = 1; i < developers.length; i++)
+//         if(developers[i]['skills'].length > _mostSkill['skills'].length) _mostSkill = developers[i];
+//     console.log(_mostSkill);
+// }
+// mostSkill();
+
+//     - 7/ Viết function tìm ra những developer nào có skill là JS
+// const skillJS = (dev) => {
+//     return dev['skills'].includes("JS");
+// };
+// console.log(developers.filter(skillJS));
+
+//     - 8/ Viết function tìm ra những developer có gender là male và có skill JS
+// const devMaleJS = (dev) => {
+//     return dev['skills'].includes("JS") && dev['gender'] == "male";
+// };
+// console.log(developers.filter(devMaleJS));
+
+//     - 9/ Viết function sắp xếp thứ tự developer có nhiều skill nhất -> ít nhất
+// console.log(developers.sort(function(a,b){return b['skills'].length-a['skills'].length;}));
