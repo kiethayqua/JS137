86d79743475b16e917e656a7c92f24ef510df0b1
HaoDuong1994 <hao.dv5370@gmail.com>
2023-03-28 20:04:18 +0700

    executing

--- day8/index.js
+++ day8/index.js
@@ -1,24 +1,81 @@
-// Find the sum of all elements.
-const arr1 = [1, 2, 3, 4, 5];
+// // Find the sum of all elements.
+// const arr1 = [1, 2, 3, 4, 5];
 
 // Remove duplicates from an array.
 let arr2 = [1, 2, 2, 4, 5, 5, 6];
+let newArr= [];
+arr2.forEach(function(number){if(!newArr.includes(number)){newArr.push(number)}})
+console.log(newArr)
 
-// Find the index of a specific element in an array.
-const arr3 = ['iOS', 'Android', 'WindowPhone'];
+// // Find the index of a specific element in an array.
+// const arr3 = ['iOS', 'Android', 'WindowPhone'];
 
-// Reverse an array.
-const arr4 = [1, 2, 3, 4, 5, 6];
+// // Reverse an array.
+// const arr4 = [1, 2, 3, 4, 5, 6];
 
-// Filter out even numbers from an array.
-const arr5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// // Filter out even numbers from an array.
+// const arr5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// Find the maximum and minimum values in an array of numbers.
-const arr6 = [1, -3, 99, 120, 50, 15];
+// // Find the maximum and minimum values in an array of numbers.
+// const arr6 = [1, -3, 99, 120, 50, 15];
 
-// Find the common elements between two arrays.
-const arr7 = [1, 2, 3, 5, 9];
-const arr8 = [2, 4, 1, 9, 0];
+// // Find the common elements between two arrays.
+// const arr7 = [1, 2, 3, 5, 9];
+// const arr8 = [2, 4, 1, 9, 0];
 
-// Map an array of numbers to their squares.
-const arr9 = [1, 2, 3, 4, 5];
+// // Map an array of numbers to their squares.
+// const arr9 = [1, 2, 3, 4, 5];
+
+// let danhTinhHao = {
+//     id: 1,
+//     name: "Hao",
+//     age: 23,
+//     School: "MindX",
+// }
+// for(let thuocTinh in danhTinhHao){
+//     console.log("đặc điểm của thuộc tính " + thuocTinh + " có giá trị là "+ danhTinhHao[thuocTinh]);
+// }
+// let a = 0
+// let b=0
+//  function nhapSo(label){
+//      let input = NaN;
+//      do{
+//          input=Number(prompt("Nhập số " + label + " vào đây nhé"));
+//           console.log(input)
+//       }while(isNaN(input));
+//       return input
+//   }
+// a = nhapSo("A")
+// b = nhapSo("B")
+// console.log(a+b)
+
+// function nhapSo(a,b){return (a+b)}
+// console.log(nhapSo(20,30))
+
+// nhapSo()
+// let input = NaN
+// function nhapSo(){
+//     do{
+//         input=prompt("Mời bạn nhập số");
+//     }while(
+//         input<0 && isNaN(input)
+//     );
+//     return input
+// }
+
+
+
+// setInterval(function(){
+//     console.log("Sau 3 giây")
+// },1000)
+
+// function workEverysecond(second,work){
+//     setTimeout(work,second);
+// }
+// function sayHello(){console.log("Xin Chào")};
+// workEverysecond(5000, sayHello);
+// console.log(sayHello)
+
+// let sum=(a,b,c)=> {return a+b+c};
+// sum(1,2,3)
+// console.log(sum(1,2,3))
--- day9/index.js
+++ day9/index.js
@@ -4,14 +4,14 @@
     Cho 1 mảng có dạng:
     - 1/ Viết function tìm ra developer lớn tuổi nhất
     - 2/ Viết function trả về danh sách developer đã được sắp xếp theo tuổi từ thấp đến cao
-    - 3/ Viết function tìm ra những developer nam
-    - 4/ Viết function tính tổng tuổi của tất cả developer
-    - 5/ Viết function tổng hợp tất cả các skills của tất cả developers trong mảng
-    - 6/ Viết function tìm ra developer có nhiều skills nhất
-    - 7/ Viết function tìm ra những developer nào có skill là JS
+    - 3/ Viết function tìm ra những developer nam/ dùng filter
+    - 4/ Viết function tính tổng tuổi của tất cả developer/ dùng reduce
+    - 5/ Viết function tổng hợp tất cả các skills của tất cả developers trong mảng/ 
+    - 6/ Viết function tìm ra developer có nhiều skills nhất. dùng skill.length
+    - 7/ Viết function tìm ra những developer nào có skill là JS ,,,dùng filter/ có inclue là js
     - 8/ Viết function tìm ra những developer có gender là male và có skill JS
     - 9/ Viết function sắp xếp thứ tự developer có nhiều skill nhất -> ít nhất
-    - 10/ Viết function trả về 1 object có dạng 
+    - 10/ Viết function trả về 1 object có dạng /// dùng filter
     {
         males: [{...}],
         females: [{...}]
@@ -61,3 +61,38 @@ const developers = [
         skills: ['Assembly', 'C', 'C++']
     }
 ];
+
+//Câu 1
+
+let ageDeveloper = developers[0].age
+ function maxAge (){
+    for(let y=0;y<developers.length;y++){
+
+    if(developers[y].age>ageDeveloper){
+        ageDeveloper=developers[y] ;
+ }
+} return ageDeveloper;
+ }
+ let maxDeveloper=maxAge();
+console.log(maxDeveloper);
+
+//Câu 3S
+
+let maleDeveloper= developers.filter(function(developer,index){return developer.gender =='male';})
+console.log(maleDeveloper)
+
+//Câu 4
+function totalAge(accumulator,index){
+    let total = accumulator + index.age
+    return total
+}
+let totalAgeDevelopers = developers.reduce(totalAge,0)
+console.log(totalAgeDevelopers)
+
+
+//Câu 2
+
+let sortAgeDevelop= developers.sort(function(a,b){
+    return a.age - b.age;
+})
+console.log(sortAgeDevelop)
