e8089e62b77c12735677ba83d0de943b2196e553
Taduynguyen <taduynguyen11@gmail.com>
2023-03-30 08:41:39 +0700

    Done

--- day9/index.js
+++ day9/index.js
@@ -61,3 +61,108 @@ const developers = [
         skills: ['Assembly', 'C', 'C++']
     }
 ];
+//Cau 1
+function findOldDev(arr=[]) {
+    if (!Array.isArray(arr) || !arr.length) return -1;
+
+    const maxAge = arr.reduce((pre, cur) =>  pre.age > cur.age ? pre : cur);
+
+    return maxAge;
+}
+
+//Cau 2
+function youngToOld(arr) {
+    if (!Array.isArray(arr) || !arr.length) return -1;
+
+    return arr.sort((a, b) => a.age - b.age);
+}
+
+//Cau 3
+function findMaleDevs(arr) {
+    if (!Array.isArray(arr) || !arr.length) return -1;
+
+    return arr.filter((dev) => dev.gender === 'male');
+}
+
+//Cau 4
+function sumAgeDevs(arr) {
+    if (!Array.isArray(arr) || !arr.length) return -1;
+
+    return arr.reduce((pre, cur) => {
+        pre + cur.age;
+    }, 0)
+}
+
+//Cau 5
+function allSkills(arr) {
+    if (!Array.isArray(arr) || !arr.length) return -1;
+
+    a = arr.reduce((skills, developer) => {
+        return skills.concat(developer.skills);
+    })
+
+    return new Set(a);
+}
+
+//Cau 6
+function findMostSkills(arr) {
+    if (!Array.isArray(arr) || !arr.length) return -1;
+
+    return arr.reduce((pre, cur) => pre.skills.length > cur.skills.length ? pre : cur);
+}
+
+//Cau 6.2
+function findMostSkills2(arr) {
+    if (!Array.isArray(arr) || !arr.length) return -1;
+
+    let mostSkillsDev = arr[0];
+    arr.forEach((developer) => {
+        if (developer.skills.length > mostSkillsDev.skills.length)
+        mostSkillsDev = developer;
+    })
+
+    return mostSkillsDev;
+}
+
+//Cau 7
+function findJsDevs(arr) {
+    if (!Array.isArray(arr) || !arr.length) return -1;
+
+    return arr.filter((developer) => developer.skills.includes('JS'));
+}
+
+//Cau 8
+function findMaleJsDevs(arr) {
+    if (!Array.isArray(arr) || !arr.length) return -1;
+
+    return arr.filter((developer) => {
+        developer.skills.includes('JS') && developer.gender === 'male'
+    });
+}
+
+//Cau 9
+function sortDevBySkills(arr) {
+    if (!Array.isArray(arr) || !arr.length) return -1;
+    
+    return arr.sort((a, b) => b.skills.length - a.skills.length);
+}
+
+//Cau 10
+function getGenderFiltered(arr) {
+    if (!Array.isArray(arr) || !arr.length) return -1;
+
+    return arr.reduce((object, developer) => {
+        if (developer.gender === 'male')
+            object.males.push(developer);
+        else
+            object.females.push(developer);
+        return object;
+    },
+        {
+        males: [],
+        females: []
+        }
+    )
+}
+
+console.log(getGenderFiltered(developers));
